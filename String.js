c8343ed25a25739a777d43fdafad7f1d8fcfa64c
Nirajan Bahadur Khadka <nirajanbahdurk@gmail.com>
2025-08-22 12:59:06 +0545

    my first commit

---
+++ String.js
@@ -0,0 +1,21 @@
+let a="hello 'niroj'";
+let b='hello"nirajan"';
+console.log(a+ b);
+//string length
+console.log(a.length);
+//Escape Character
+let z="It\'s me nirajan khadka.";
+let y="I\'m from sindhuli.";
+console.log(z+ y);
+console.log(a.charAt(4));
+// string as object
+let d= new String("ranjeet");
+console.log(d);
+console.log(typeof(d));
+//concat
+console.log(a.concat(b));
+//endsWith()
+let e="new road";
+let f="new time";
+console.log(e.endsWith("road"));
+console.log(f.endsWith("road"));
---
+++ typof.js
@@ -0,0 +1,18 @@
+let x=123;
+let  f= function(){
+    console.log("function")
+}
+let name="Nirajan";
+let obj={
+    name:"NIroj",
+    age:22,
+};
+console.log(typeof(x));
+console.log(typeof(f));
+console.log(typeof(name));
+console.log(typeof(obj));
+console.log(typeof(undefined));
+console.log(typeof(new Date()));
+
+
+
